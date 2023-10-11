d00dd5e5e1d13a683cc3f3d08702521587efab49
jessi <jpatri02@students.cs.ubc.ca>
2023-10-11 05:33:54 -0700

    Phase 1

--- src/main/model/Item.java
+++ src/main/model/Item.java
@@ -1,35 +1,31 @@
 package model;
 
-//represents an item having a name, quantity, buy and sell prices,
-//
+//represents an item having a name, quantity, and minimum stock limit
 public class Item {
     private String name;
     private int quantity;
     private int minimumStockLimit;
-    //    private double buyPrice;
-//    private double sellPrice;
-    private boolean isLowStock;
 
-
+    //EFFECTS: constructs an item with given name, a quantity of 0, and a minimum stock limit of 0
     public Item(String name) {
         this.name = name;
         this.quantity = 0;
-        this.isLowStock = true;
-//        this.buyPrice = 0;
-//        this.sellPrice = 0;
+        this.minimumStockLimit = 0;
     }
 
+    //REQUIRES: amount >= 0
+    //MODIFIES: this
+    //EFFECTS: adds quantity of item by amount
     public void addQuantity(int amount) {
         this.quantity += amount;
     }
 
-//    public void setBuyPrice(double buyPrice) {
-//        this.buyPrice = buyPrice;
-//    }
-//
-//    public void setSellPrice(double sellPrice) {
-//        this.sellPrice = sellPrice;
-//    }
+    //EFFECTS: checks if item is low in stock.
+    //         Returns true if quantity is less than minimum stock limit.
+    //         Returns false if quantity is greater than or equal to minimum stock limit
+    public boolean isLowStock() {
+        return (quantity < minimumStockLimit);
+    }
 
     public void setQuantity(int quantity) {
         this.quantity = quantity;
@@ -37,14 +33,6 @@ public class Item {
 
     public void setMinimumStockLimit(int minimumStockLimit) {
         this.minimumStockLimit = minimumStockLimit;
-    }
-
-    public boolean isLowStock() {
-        if (quantity < minimumStockLimit) {
-            return true;
-        } else {
-            return false;
-        }
     }
 
     public String getName() {
